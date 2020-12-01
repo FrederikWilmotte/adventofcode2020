08b22b051ad0e22123604b78939267a2dbd11b6a
Frederik Wilmotte <frederik.wilmotte@dxcfds.com>
2020-12-01 09:59:48 +0100

    Added Solution for first day

---
+++ 20201201/aoc20201201pt1.py
@@ -0,0 +1,27 @@
+# Advent of Code 2020
+# Author: Frederik Wilmotte
+# --- Day 1: Report Repair ---
+# --- Part One ---
+
+expenseReport = open("expense_report", "r")
+expenses = []
+address1 = 0
+address2 = 1
+expenseSum = 0
+expenseMultiply = 0
+
+# Load expenses in array
+for expense in expenseReport:
+    expenses.append(int(expense))
+
+expenseSum = expenses[address1] + expenses[address2]
+while expenseSum != 2020:
+    address2 += 1
+    if address2 == len(expenses):
+        address1 += 1
+        address2 = address1 + 1
+    expenseSum = expenses[address1] + expenses[address2]
+
+expenseMultiply = expenses[address1] * expenses[address2]
+
+print("expenseMultiply:", expenseMultiply)
---
+++ 20201201/aoc20201201pt2.py
@@ -0,0 +1,31 @@
+# Advent of Code 2020
+# Author: Frederik Wilmotte
+# --- Day 1: Report Repair ---
+# --- Part Two ---
+
+expenseReport = open("expense_report", "r")
+expenses = []
+address1 = 0
+address2 = 1
+address3 = 2
+expenseSum = 0
+expenseMultiply = 0
+
+# Load expenses in array
+for expense in expenseReport:
+    expenses.append(int(expense))
+
+expenseSum = expenses[address1] + expenses[address2] + expenses[address3]
+while expenseSum != 2020:
+    address3 += 1
+    if address3 == len(expenses):
+        address2 += 1
+        if address2 == len(expenses) - 1:
+            address1 += 1
+            address2 = address1 + 1
+        address3 = address2 + 1
+    expenseSum = expenses[address1] + expenses[address2] + expenses[address3]
+
+expenseMultiply = expenses[address1] * expenses[address2] * expenses[address3]
+
+print("expenseMultiply:", expenseMultiply)
