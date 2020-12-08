ad849eb54e7e92758d04d226e341498206105d4b
Frederik Wilmotte <frederik.wilmotte@dxcfds.com>
2020-12-08 12:50:03 +0100

    Added solutions and dodona solutions for day 8

---
+++ 20201208/dodona/aoc20201208pt1.py
@@ -0,0 +1,39 @@
+# Advent of Code 2020
+# Author: Frederik Wilmotte
+# --- Day 8: Handheld Halting ---
+# --- Part One ---
+# Dodona submission
+
+
+def inspect(boot_code):
+    boot_code_instructions = open(boot_code, "r")
+    instructions = []
+    code_executed = []
+    for boot_code_instruction in boot_code_instructions:
+        instructions.append(boot_code_instruction)
+        code_executed.append(False)
+    acc = 0
+    pos = 0
+    while not code_executed[pos]:
+        code_executed[pos] = True
+        (operation, argument) = read_instruction(instructions[pos])
+        (acc, pos) = process_instruction(acc, pos, operation, argument)
+    return acc
+
+
+def read_instruction(instruction):
+    split_instruction = instruction.split()
+    operation = split_instruction[0]
+    argument = int(split_instruction[1])
+    return operation, argument
+
+
+def process_instruction(acc, pos, operation, argument):
+    if operation == "nop":
+        pos += 1
+    elif operation == "acc":
+        pos += 1
+        acc += argument
+    elif operation == "jmp":
+        pos += argument
+    return acc, pos
---
+++ 20201208/dodona/aoc20201208pt2.py
@@ -0,0 +1,75 @@
+# Advent of Code 2020
+# Author: Frederik Wilmotte
+# --- Day 8: Handheld Halting ---
+# --- Part One ---
+# Dodona submission
+
+
+def fix(boot_code):
+    boot_code_instructions = open(boot_code, "r")
+    instructions = []
+    code_executed = []
+    for boot_code_instruction in boot_code_instructions:
+        instructions.append(boot_code_instruction.replace("\n", ""))
+        code_executed.append(False)
+    code_executed.append(True)
+    acc = 0
+    pos = 0
+    last_switch = -1
+    while pos < len(instructions):
+        acc = 0
+        pos = 0
+        code_executed_temp = code_executed.copy()
+        while not code_executed_temp[pos]:
+            code_executed_temp[pos] = True
+            (operation, argument) = read_instruction(instructions[pos])
+            (acc, pos) = process_instruction(acc, pos, operation, argument)
+        if pos < len(instructions):
+            (instructions, last_switch) = check_switch_instruction(instructions, last_switch)
+    return acc
+
+
+def read_instruction(instruction):
+    split_instruction = instruction.split()
+    operation = split_instruction[0]
+    argument = int(split_instruction[1])
+    return operation, argument
+
+
+def process_instruction(acc, pos, operation, argument):
+    if operation == "nop":
+        pos += 1
+    elif operation == "acc":
+        pos += 1
+        acc += argument
+    elif operation == "jmp":
+        pos += argument
+    return acc, pos
+
+
+def check_switch_instruction(instructions, old_switch):
+    new_switch = -1
+    if old_switch >= 0:
+        switch_instruction(instructions, old_switch)
+        old_switch += 1
+    else:
+        old_switch = 0
+    while new_switch < 0:
+        switched = switch_instruction(instructions, old_switch)
+        if switched:
+            new_switch = old_switch
+        else:
+            old_switch += 1
+    return instructions, new_switch
+
+
+def switch_instruction(instructions, switch_position):
+    switched = False
+    (switch_operation, switch_argument) = read_instruction(instructions[switch_position])
+    if switch_operation == "nop":
+        instructions[switch_position] = instructions[switch_position].replace("nop", "jmp")
+        switched = True
+    elif switch_operation == "jmp":
+        instructions[switch_position] = instructions[switch_position].replace("jmp", "nop")
+        switched = True
+    return switched
