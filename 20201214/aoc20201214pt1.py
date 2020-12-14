61795a1a99ebbb54c149734fb7ecbced15d993db
Frederik Wilmotte <frederik.wilmotte@dxcfds.com>
2020-12-14 11:45:44 +0100

    Added solution (also dodona) for day 14 part 1

---
+++ 20201214/aoc20201214pt1.py
@@ -0,0 +1,48 @@
+# Advent of Code 2020
+# Author: Frederik Wilmotte
+# --- Day 14: Docking Data ---
+# --- Part One ---
+
+
+def memory(program_file):
+    program = read_program(program_file)
+    mem = [0 for _ in range(100000)]
+    mask = ""
+    sum_values = 0
+    for program_line in program:
+        program_line_split = program_line.split(" = ")
+        if program_line_split[0] == "mask":
+            mask = program_line_split[1]
+        else:
+            value = int(program_line_split[1])
+            result = bitmask(value, mask)
+            program_line_split[0] = program_line_split[0].replace("mem[", "")
+            program_line_split[0] = program_line_split[0].replace("]", "")
+            mem[int(program_line_split[0])] = result
+    for values in mem:
+        sum_values += values
+    return sum_values
+
+
+def bitmask(value, mask):
+    value_bit = "{0:036b}".format(value)
+    result_bit = ""
+    for i in range(len(mask)):
+        if mask[i] == "X":
+            result_bit = result_bit + value_bit[i]
+        else:
+            result_bit = result_bit + mask[i]
+    result = int(result_bit, 2)
+    return result
+
+
+def read_program(program_file):
+    program_output = open(program_file, "r")
+    program = []
+    for program_line in program_output:
+        program.append(program_line.replace("\n", ""))
+    return program
+
+
+print(memory("program_test"))
+print(memory("program"))
